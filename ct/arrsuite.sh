89f6af4ae54dd230c6c3e7ebd8f1208b1a8b19ff
Gregory Scales <244308@esj.org>
2026-04-04 18:13:34 -0400

    feat: Create ct/arrsuite.sh from ct/gluetun.sh, updating APP name and t…
    - ct/arrsuite.sh
    
    GSD-Task: S01/T01

---
+++ ct/arrsuite.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2026 community-scripts ORG
+# Author: MickLesk (CanbiZ)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/qdm12/gluetun
+
+APP="Arrsuite"
+var_tags="arrsuite;vpn"
+var_cpu="${var_cpu:-2}"
+var_ram="${var_ram:-2048}"
+var_disk="${var_disk:-8}"
+var_os="${var_os:-debian}"
+var_version="${var_version:-13}"
+var_unprivileged="1"
+var_tun="yes"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+
+  msg_error "Update script not yet implemented for Arrsuite."
+  exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
